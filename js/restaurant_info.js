d0045d07c61cc2e68856ee2ac39caef61f1a4ce5
nadjayanna <nadjayannabr@gmail.com>
2018-09-29 10:07:22 +0200

    add mapbox token to restaurants info

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoibmFkamF5YW5uYSIsImEiOiJjam1obXNsc3EwYTB4M3FycjVhdmRrMzVoIn0.eWeCIP1-J4HF842vprpnRQ',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
