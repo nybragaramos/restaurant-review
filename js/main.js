f48729bdebf0684f4b30035a3f58109f9161e23f
nadjayanna <nadjayannabr@gmail.com>
2018-09-29 18:43:11 +0200

    adding HTML5 tags to restaurantes list

--- js/main.js
+++ js/main.js
@@ -132,8 +132,8 @@ updateRestaurants = () => {
 resetRestaurants = (restaurants) => {
   // Remove all restaurants
   self.restaurants = [];
-  const ul = document.getElementById('restaurants-list');
-  ul.innerHTML = '';
+  const restaurantsList = $('.restaurants');
+  restaurantsList.innerHTML = '';
 
   // Remove all map markers
   if (self.markers) {
@@ -147,9 +147,9 @@ resetRestaurants = (restaurants) => {
  * Create all restaurants HTML and add them to the webpage.
  */
 fillRestaurantsHTML = (restaurants = self.restaurants) => {
-  const ul = document.getElementById('restaurants-list');
+  const restaurantsSection = $('.restaurants');
   restaurants.forEach(restaurant => {
-    ul.append(createRestaurantHTML(restaurant));
+    restaurantsSection.append(createRestaurantHTML(restaurant));
   });
   addMarkersToMap();
 }
@@ -158,31 +158,43 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  * Create restaurant HTML.
  */
 createRestaurantHTML = (restaurant) => {
-  const li = document.createElement('li');
+  const article = document.createElement('article');
+  article.className = 'restaurant';
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  li.append(image);
+  article.append(image);
+
+  const header = document.createElement('header');
+  header.className = 'restaurant-name';
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  
+  header.append(name);
+  article.append(header);
+
+  const info = document.createElement('section');
+  info.className = 'restaurant-info';
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  info.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  info.append(address);
+
+  article.append(info);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  more.setAttribute('aria-label', `View Details from ${restaurant.name}`);
+  article.append(more)
 
-  return li
+  return article
 }
 
 /**
