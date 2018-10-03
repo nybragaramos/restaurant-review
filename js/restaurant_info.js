dba4899b15d60a8cf65845d6b21ede655a22bfff
nadjayanna <nadjayannabr@gmail.com>
2018-10-03 15:46:29 +0200

    add style to reviews

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -147,18 +147,22 @@ createReviewHTML = (review) => {
   li.className = 'reviews-list-item';
 
   const name = document.createElement('p');
+  name.className = 'review-name';
   name.innerHTML = review.name;
   li.appendChild(name);
 
   const date = document.createElement('p');
+  date.className = 'review-date';
   date.innerHTML = review.date;
   li.appendChild(date);
 
   const rating = document.createElement('p');
+  rating.className = 'review-rate';
   rating.innerHTML = `Rating: ${review.rating}`;
   li.appendChild(rating);
 
   const comments = document.createElement('p');
+  comments.className = 'review-comment';
   comments.innerHTML = review.comments;
   li.appendChild(comments);
 
